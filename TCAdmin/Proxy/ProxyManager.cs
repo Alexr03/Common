b373416435d2c246438d870c5306c2beda71c389
Alex Redding <Alexredding180@gmail.com>
2020-09-12 17:44:08 +0100

    Initial

---
+++ TCAdmin/Proxy/ProxyManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Alexr03.Common.TCAdmin.Permissions;
+using TCAdmin.SDK;
+using TCAdmin.SDK.Proxies;
+
+namespace Alexr03.Common.TCAdmin.Proxy
+{
+    public static class ProxyManager
+    {
+        public static readonly List<CommandProxy> CommandProxies = new List<CommandProxy>();
+
+        public static void AddProxy(this CommandProxy commandProxy)
+        {
+            CommandProxies.Add(commandProxy);
+        }
+
+        public static void RemoveProxy(this CommandProxy commandProxy)
+        {
+            CommandProxies.Remove(commandProxy);
+        }
+
+        public static void RegisterProxies()
+        {
+            foreach (var commandProxy in CommandProxies)
+            {
+                commandProxy.RegisterProxy();
+            }
+        }
+        
+        public static void RegisterProxy(this CommandProxy commandProxy)
+        {
+            if (!CommandProxies.Contains(commandProxy))
+            {
+                CommandProxies.Add(commandProxy);
+            }
+            
+            AppDomainManager.RegisterProxyCommand(commandProxy);
+        }
+        
+        public static void UnRegisterProxies()
+        {
+            foreach (var commandProxy in CommandProxies)
+            {
+                if (CommandProxies.Contains(commandProxy))
+                {
+                    CommandProxies.Remove(commandProxy);
+                }
+                UnRegisterProxy(commandProxy.CommandName);
+            }
+        }
+
+        public static void UnRegisterProxy(string commandName)
+        {
+            if (CommandProxies.Any(x => x.CommandName == commandName))
+            {
+                CommandProxies.RemoveAll(x => x.CommandName == commandName);
+            }
+            AppDomainManager.UnregisterProxyCommand(commandName);
+        }
+    }
+
+    public enum Oof
+    {
+        help,
+        me
+    }
+}
---
+++ TCAdmin/Proxy/Requests/ProxyRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TCAdmin.SDK.Misc;
+using TCAdmin.SDK.Web.References.ModuleApiGateway;
+using Server = TCAdmin.GameHosting.SDK.Objects.Server;
+
+namespace Alexr03.Common.TCAdmin.Proxy.Requests
+{
+    public static class ProxyRequest
+    {
+        public static T Perform<T>(string commandName,
+            object arguments, out CommandResponse commandResponse, bool waitForResponse = true)
+        {
+            try
+            {
+                var server = Server.GetServerFromCache(1);
+                commandResponse = new CommandResponse();
+                server.ModuleApiGateway.ExecuteModuleCommand(commandName, arguments, ref commandResponse,
+                    waitForResponse);
+
+                var xmlToObject = (T) ObjectXml.XmlToObject(commandResponse.Response.ToString(), typeof(T));
+                return xmlToObject;
+            }
+            catch (Exception e)
+            {
+                commandResponse = new CommandResponse {SerializedException = e.Message};
+                return Activator.CreateInstance<T>();
+            }
+        }
+    }
+}
