889f2094d30dbf89fd7d24cf220fc6bcb94e3f7a
Alex Redding <Alexredding180@gmail.com>
2020-09-20 19:09:50 +0100

    Better logger

--- Logging/Logger.cs
+++ Logging/Logger.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Microsoft.Extensions.Logging;
+using System.IO;
 using Serilog;
 using Serilog.Events;
 
@@ -7,66 +7,62 @@ namespace Alexr03.Common.Logging
 {
     public class Logger
     {
+        private const string LogBaseLocation = "./Components/{0}/Logs/{1}/{2}/{2}.log";
         public string Application { get; }
-
         public Serilog.Core.Logger InternalLogger { get; }
+        internal Type Type { get; set; }
 
-        public Logger(string application)
+        public Logger(string application, Type type = null)
         {
+            Console.WriteLine(2);
             Application = application;
+            Type = type;
 
             var consoleOutputTemplate =
                 $"[{application}" + " {Timestamp:HH:mm:ss.ff} {Level:u3}] {Message:lj}{NewLine}{Exception}";
             var loggerConfiguration = new LoggerConfiguration()
                 .MinimumLevel.Debug()
-                .WriteTo.Console(outputTemplate: consoleOutputTemplate)
-                .WriteTo.File($"./Logs/{application}/{application}.log", rollingInterval: RollingInterval.Day);
+                .WriteTo.Console(outputTemplate: consoleOutputTemplate);
+            if (Type != null)
+            {
+                var assemblyName = Type.Assembly.GetName().Name;
+                var logLocation = LogBaseLocation.Replace("{0}",
+                        Path.Combine(assemblyName, Type.Namespace?
+                                .Replace(assemblyName, "") ?? "")
+                            .Replace(".", ""))
+                    .Replace("{1}", Type?.Name)
+                    .Replace("{2}", application);
+                loggerConfiguration.WriteTo.File(logLocation, rollingInterval: RollingInterval.Day);
+            }
+            else
+            {
+                loggerConfiguration.WriteTo.File($"./Components/Misc/Logs/{application}/{application}.log",
+                    rollingInterval: RollingInterval.Day);
+            }
 
             InternalLogger = loggerConfiguration.CreateLogger();
         }
 
+        public static Logger Create<T>(string application)
+        {
+            var logger = new Logger(application, typeof(T));
+            return logger;
+        }
+
         public static Logger Create<T>()
         {
-            return new Logger(typeof(T).Assembly.GetName().Name);
+            var logger = new Logger(typeof(T).Assembly.GetName().Name, typeof(T));
+            return logger;
         }
 
         public void LogMessage(string message)
         {
-            LogMessage(LogLevel.Information, message);
+            LogMessage(LogEventLevel.Information, message);
         }
 
         public void LogDebugMessage(string message)
         {
-            LogMessage(LogLevel.Debug, message);
-        }
-
-        public void LogMessage(LogLevel logLevel, string message)
-        {
-            switch (logLevel)
-            {
-                case LogLevel.Critical:
-                    InternalLogger.Fatal(message);
-                    LogReceived?.Invoke(LogEventLevel.Fatal, InternalLogger, message, Application);
-                    break;
-                case LogLevel.Debug:
-                    InternalLogger.Debug(message);
-                    LogReceived?.Invoke(LogEventLevel.Debug, InternalLogger, message, Application);
-                    break;
-                case LogLevel.Information:
-                    InternalLogger.Information(message);
-                    LogReceived?.Invoke(LogEventLevel.Information, InternalLogger, message, Application);
-                    break;
-                case LogLevel.Warning:
-                    InternalLogger.Warning(message);
-                    LogReceived?.Invoke(LogEventLevel.Warning, InternalLogger, message, Application);
-                    break;
-                case LogLevel.Error:
-                    InternalLogger.Error(message);
-                    LogReceived?.Invoke(LogEventLevel.Error, InternalLogger, message, Application);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(logLevel), logLevel, null);
-            }
+            LogMessage(LogEventLevel.Debug, message);
         }
 
         public void LogMessage(LogEventLevel logLevel, string message)
