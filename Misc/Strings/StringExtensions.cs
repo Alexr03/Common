155e1f3ed06a98ca40229b0ba43a70545091af5d
Alex Redding <Alexredding180@gmail.com>
2020-10-06 15:33:38 +0100

    Initial

--- Misc/Strings/StringExtensions.cs
+++ Misc/Strings/StringExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Alexr03.Common.Misc.Strings
 {
@@ -12,6 +13,34 @@ namespace Alexr03.Common.Misc.Strings
                  (?<=[A-Za-z])(?=[^A-Za-z])", RegexOptions.IgnorePatternWhitespace);
 
             return regex.Replace(s, " ");
+        }
+
+        public static string Base64Encode(this string s)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(s);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string Base64Decode(this string s)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(s);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+        
+        public static string Md5Encode(this string input)
+        {
+            using (var md5 = System.Security.Cryptography.MD5.Create())
+            {
+                var inputBytes = Encoding.ASCII.GetBytes(input);
+                var hashBytes = md5.ComputeHash(inputBytes);
+
+                var sb = new StringBuilder();
+                foreach (var t in hashBytes)
+                {
+                    sb.Append(t.ToString("X2"));
+                }
+                return sb.ToString();
+            }
         }
     }
 }
