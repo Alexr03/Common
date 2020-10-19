3ef04e8d476a7bd2225a0b008fef0a20fa2e972a
Alex Redding <Alexredding180@gmail.com>
2020-10-19 23:16:41 +0100

    Fix

--- TCAdmin/Objects/DynamicTypeBase.cs
+++ TCAdmin/Objects/DynamicTypeBase.cs
@@ -136,7 +136,7 @@ namespace Alexr03.Common.TCAdmin.Objects
             return (T) GetCurrent(typeof(T), idParam);
         }
 
-        private static string RequestBody(Stream stream)
+        public static string RequestBody(Stream stream)
         {
             var bodyStream = new StreamReader(stream);
             bodyStream.BaseStream.Seek(0, SeekOrigin.Begin);
